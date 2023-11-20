7374c04ac828fe487735e5dc481b78ce874f2811
Ariel Curtido <aicurtido@gmail.com>
2023-11-20 00:30:08 -0300

    Ajustes footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,41 +7,43 @@ import imagenEmail from '/icons/icon-email.svg'
 export default function Footer() {
     return (
         <div className='footer'>
-            <img src={imagenFooter} alt="imagen-footer" />
-            <div className='footer__content'>
-                <div className='footer__location'>
-                    <div className="footer__info">
-                        <img src={imagenLocation} alt="imagen-location" />
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam reiciendis officiis vel blanditiis, rerum consequatur ut. Reprehenderit aliquid, alias consectetur iusto ipsum quisquam dicta, quo mollitia eligendi atque debitis ex!</p>
+            <div className="footer__content">
+                <img src={imagenFooter} alt="imagen-footer" />
+                <div className='footer__sections'>
+                    <div className='footer__location'>
+                        <div className="footer__info">
+                            <img src={imagenLocation} alt="imagen-location" />
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam reiciendis officiis vel blanditiis, rerum consequatur ut. Reprehenderit aliquid, alias consectetur iusto ipsum quisquam dicta, quo mollitia eligendi atque debitis ex!</p>
+                        </div>
                     </div>
-                </div>
-                <div className='footer__contact'>
-                    <div className="footer__info">
-                        <img src={imagenPhone} alt="" />
-                        <p> +1-543-123-4567 </p>
+                    <div className='footer__contact'>
+                        <div className="footer__info">
+                            <img src={imagenPhone} alt="" />
+                            <p> +1-543-123-4567 </p>
+                        </div>
+                        <div className="footer__info">
+                            <img src={imagenEmail} alt="" />
+                            <p>example@fylo.com</p>
+                        </div>
                     </div>
-                    <div className="footer__info">
-                        <img src={imagenEmail} alt="" />
-                        <p>example@fylo.com</p>
+                    <div className='footer__links'>
+                        <ul>
+                            <li>About Us</li>
+                            <li>Jobs</li>
+                            <li>Press</li>
+                            <li>Blog</li>
+                        </ul>
+                        <ul>
+                            <li>Contact Us</li>
+                            <li>Term</li>
+                            <li>Privacy</li>
+                        </ul>
                     </div>
-                </div>
-                <div className='footer__links'>
-                    <ul>
-                        <li>About Us</li>
-                        <li>Jobs</li>
-                        <li>Press</li>
-                        <li>Blog</li>
-                    </ul>
-                    <ul>
-                        <li>Contact Us</li>
-                        <li>Term</li>
-                        <li>Privacy</li>
-                    </ul>
-                </div>
-                <div className='footer__socials'>
-                    <i className="fab fa-facebook-square"></i>
-                    <i className="fab fa-twitter"></i>
-                    <i className="fab fa-instagram"></i>
+                    <div className='footer__socials'>
+                        <i className="fab fa-facebook-square"></i>
+                        <i className="fab fa-twitter"></i>
+                        <i className="fab fa-instagram"></i>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,16 +3,23 @@ import './testimonials.css';
 
 export default function Testimonials() {
     return (
-        testimonials.map((testimonial, index) => {
-            return (
-                <div className="testimonial" key={index}>
-                    <p className="testimonial__quotes">{testimonial.quote}</p>
-                    <div className="testimonials__info">
-                        <img className="testimonial__image" src={testimonial.image} alt="" />
-                        <h2 className="testimonial__name">{testimonial.name}</h2>
-                        <h3 className="testimonial__title">{testimonial.title}</h3>
-                    </div>
-                </div>
-            )
-        }))
+        <div className="testimonials">
+            <div className="testimonials__content">
+                {testimonials.map((testimonial, index) => {
+                    return (
+                        <div className="testimonials__item" key={index}>
+                            <p className="testimonial__quote">{testimonial.quote}</p>
+                            <div className="testimonial__info">
+                                <img className="testimonial__image" src={testimonial.image} alt="" />
+                                <div className="testimonial__text">
+                                    <h2 className="testimonial__name">{testimonial.name}</h2>
+                                    <h3 className="testimonial__title">{testimonial.title}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
