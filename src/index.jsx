de9f8c103c51d13579b7c14cdc9fd833d91a90eb
Ariel Curtido <aicurtido@gmail.com>
2023-11-22 12:49:20 -0300

    Early Access component

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import StayProductive from './components/StayProductive'
 import Footer from './components/Footer'
 import Features from './components/Features'
 import Testimonials from './components/Testimonials'
+import EarlyAccess from './components/EarlyAccess'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Features />
     <StayProductive />
     <Testimonials />
+    <EarlyAccess />
     <Footer />
   </React.StrictMode>,
 )
