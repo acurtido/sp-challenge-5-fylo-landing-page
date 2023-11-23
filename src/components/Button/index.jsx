4c0c77aa59a1333b464fd07562146a552e4477f7
Ariel Curtido <aicurtido@gmail.com>
2023-11-23 15:14:26 -0300

    Ajustes Estilos

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './styles.css'
+
+export default function Button() {
+  return (
+    <div>Button</div>
+  )
+}
--- src/components/EarlyAccess/index.jsx
+++ src/components/EarlyAccess/index.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import './styles.css'
 
 export default function EarlyAccess() {
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
     return (
         <div className="container">
             <div className='box'>
                 <h1>Get early access today</h1>
-                <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</p>
-                <input type="email" />
-                <button>Get Started For Free</button>
+                <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have <br/> any questions, our support team would be happy to help you.</p>
+                <form className='formulario' onSubmit={handleSubmit}>
+                    <input type="email" />
+                    <button>Get Started For Free</button>
+                </form>
             </div>
         </div>
     )
