6f95a68a693b200dd4c3d09d318b7ffe0af97b61
Ariel Ignacio Curtido García <aicurtido@gmail.com>
2023-11-23 20:54:44 -0300

    Ajustes CSS

--- src/components/EarlyAccess/index.jsx
+++ src/components/EarlyAccess/index.jsx
@@ -15,7 +15,7 @@ export default function EarlyAccess() {
         <div className={styles.earlyAccess}>
             <div className={styles.earlyAccess__container}>
                 <h1>Get early access today</h1>
-                <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have <br />
+                <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have   
                     any questions, our support team would be happy to help you.</p>
                 <form className={styles.formulario} onSubmit={handleSubmit}>
                     <InputMail ancho={300} onChange={e => setMail(e.target.value)} />
